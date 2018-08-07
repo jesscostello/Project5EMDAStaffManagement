8b7a75235453490fd15d4ee04101165cac2db8cb
Jess Costello <jessica.a.costello@gmail.com>
2018-08-08 11:49:31 +1200

    create reasons

--- Project5EMDAStaffManagement/Controllers/ReasonsController.cs
+++ Project5EMDAStaffManagement/Controllers/ReasonsController.cs
@@ -54,7 +54,7 @@ namespace Project5EMDAStaffManagement.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Reason,ReasonCount")] Reasons reasons)
+        public async Task<IActionResult> Create([Bind("Id,Reason")] Reasons reasons)
         {
             if (ModelState.IsValid)
             {
